f6bb14b6b0720d7b492fcc34746893890a6b6a7b
seasons@traveality.com <seasons@traveality.com>
2026-05-02 18:53:50 +0100

    feature:custom error page

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Alf's Cycle - Page Not Found</title>
+  <link rel="stylesheet" href="./assets/css/styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Libre+Baskerville:ital,wght@0,400..700;1,400..700&display=swap"
+    rel="stylesheet">
+
+</head>
+
+<body>
+  <?php include 'layout/header.php'; ?>
+
+  <main class="content">
+    <section class="error-container">
+      <h1>404</h1>
+      <h2>Oops! We lost the trail.</h2>
+      <p>
+        The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.
+      </p>
+      <div>
+        <a href="./index.php" class="primary-link">Pedal Back Home</a>
+      </div>
+    </section>
+  </main>
+
+  <?php include 'layout/footer.php'; ?>
+</body>
+
+</html>
